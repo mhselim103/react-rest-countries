2ad67fa5616fec9f26b0f35a493b054d34fd8386
mhselim103 <mhselin103@gmail.com>
2021-10-16 12:58:10 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import Countries from './Components/Countries/Countries';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Countries></Countries>
     </div>
   );
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function Countries() {
+//   const [countries, setCountries] = useState([]);
+//   useEffect(() => {
+//     fetch('https://raw.githubusercontent.com/ProgrammingHero1/rest-countries-data/main/coutries.JSON')
+//     .then(res=>res.json())
+//     .then(data=> setCountries(data))
+//   },[])
+//   return (
+//     <div>
+//       <h1>Travelling Around the World</h1>
+//       <h3>Country Available: {countries.length}</h3>
+//       {
+//         countries.map(country => <Country name={country.name}></Country>)
+//       }
+//     </div>
+//   )
+// }
+
+// function Country(props) {
+//   return (
+//     <div>
+//       <h3>Country Name:{props.name}</h3>
+//     </div>
+//   )
+// }
 export default App;
---
+++ src/Components/Countries/Countries.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Countries = () => {
+    
+    
+    return (
+        <div>
+            <h1>Hello From inside Countries</h1>
+        </div>
+    );
+};
+
+export default Countries;
